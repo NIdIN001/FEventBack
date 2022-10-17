2cd0d396f8e637a183fde0115482619dd870dca1
Utochkin Stas <utochkins@bk.ru>
2022-10-17 16:28:46 +0700

    сделал часть полей UserDto null-safe

--- src/main/kotlin/ru/nsu/fevent/dto/UserDto.kt
+++ src/main/kotlin/ru/nsu/fevent/dto/UserDto.kt
@@ -2,8 +2,8 @@ package ru.nsu.fevent.dto
 
 data class UserDto(
     val login: String,
-    val firstname: String?,
-    val lastName: String?,
+    val firstname: String,
+    val lastName: String,
     val city: String?,
     val email: String?,
     val createdAt: String
