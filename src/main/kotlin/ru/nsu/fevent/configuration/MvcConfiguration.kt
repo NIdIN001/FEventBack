7260a76512f7e2e441b5188b8365ef4d19641a49
Utochkin Stas <utochkins@bk.ru>
2022-11-05 21:28:05 +0700

    добавил подключаемые метрики

--- src/main/kotlin/ru/nsu/fevent/configuration/MvcConfiguration.kt
+++ src/main/kotlin/ru/nsu/fevent/configuration/MvcConfiguration.kt
@@ -1,11 +1,13 @@
 package ru.nsu.fevent.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import ru.nsu.fevent.controller.interceptor.AuthenticationInterceptor
 
 @Configuration
+@EnableWebMvc
 class MvcConfiguration(
     private val authenticationInterceptor: AuthenticationInterceptor,
 ) : WebMvcConfigurer {
@@ -16,6 +18,10 @@ class MvcConfiguration(
         /* Формирование защиненных ресурсов, доступ к которым разрешен только аутентифицированным пользователям */
         registry.addInterceptor(authenticationInterceptor)
             .addPathPatterns("/**")
-            .excludePathPatterns("/user/register", "/authenticate/login")
+            .excludePathPatterns(
+                "/user/register",
+                "/authenticate/login",
+                "/actuator/**"
+            )
     }
 }
