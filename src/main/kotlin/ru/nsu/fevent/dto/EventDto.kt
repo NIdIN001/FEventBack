ef5c6902f145faa75386b57fbcb1145a399ff852
DanielKlyus <77263885+DanielKlyus@users.noreply.github.com>
2022-11-07 01:21:23 +0700

    Update EventDto.kt

--- src/main/kotlin/ru/nsu/fevent/dto/EventDto.kt
+++ src/main/kotlin/ru/nsu/fevent/dto/EventDto.kt
@@ -1,4 +1,15 @@
 package ru.nsu.fevent.dto
 
-class EventDto {
-}
+data class EventDto(
+    val name: String,
+    val description: String?,
+    val datetimeStart: String,
+    val datetimeEnd: String?,
+    val address: String,
+    val maxMembers: Int?,
+    val ageMin: Int?,
+    val ageMax: Int?,
+    val isOnline: Boolean,
+    val isPrivate: Boolean,
+    val creatorId: Int?
+)
